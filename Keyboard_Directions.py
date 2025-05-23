105df66f4ae760a40e33742c31491062e7e20da5
FranckSimo314 <dzessifranck@gmail.com>
2025-05-23 12:32:05 +0200

    Add files via upload
    fichier de pilotage de marty avec Clavier

---
+++ Keyboard_Directions.py
@@ -0,0 +1,23 @@
+import sys
+from PyQt6.QtWidgets import QApplication, QLabel, QVBoxLayout, QWidget
+from PyQt6.QtCore import Qt
+from Marty_Detection import move_on
+from Marty_Detection import move_back
+from Marty_Detection import step_right
+from Marty_Detection import step_left
+
+
+
+def keyPressEvent(event):
+    if event.key() == Qt.Key.Key_Z:
+        ##J'appelle la fonction dans un autre fichier 
+        move_on()
+    elif event.key() == Qt.Key.Key_S:
+        move_back()
+    elif event.key() == Qt.Key.Key_Q:
+        step_right()
+    elif event.key() == Qt.Key.Key_D:
+        step_left()
+    
+
+
