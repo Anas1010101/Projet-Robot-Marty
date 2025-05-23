9c8bcb10fcdf427239188b1453eb0fda12d47e8d
Anas <131682794+Anas1010101@users.noreply.github.com>
2025-05-23 12:46:16 +0200

    Ajout de la fonction de deconnexion, finalisation

--- MartyProject/martyProject.py
+++ MartyProject/martyProject.py
@@ -1,16 +1,50 @@
 from martypy import Marty
+import time
+
+marty = Marty("wifi", "192.168.0.102")
+
 
 MARTY_IP = "192.168.0.102"
 
+
+
 def connexion():
+    
     try:
+        print("Connexion réussie à Marty.")
         marty = Marty("wifi", MARTY_IP)
+        marty.enable_motors()
+        marty.hello()
         marty.dance()
-        print("Connexion Réussie à Marty.")
+        marty.play_mp3("soundLinga.mp3")
+
+        
+        
     except Exception as e:
         print(f"Erreur de connexion : {e}")
 
-if __name__ == "__main__":
-    connexion()
+
+#Cette fonction assure la deconnexion 
+def deconnexion():
+    global marty
+    if marty:
+        try:
+            marty.close()
+            print("✅ Déconnexion réussie.")
+        except Exception as e:
+            print(f"⚠️ Erreur lors de la déconnexion : {e}")
+
 
     
+    
+    
+
+if __name__ == "__main__":
+    connexion()
+    
+    deconnexion()
+    
+    
+    
+
+
