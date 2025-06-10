920b90f555474a692d91f73d43a94493b6d4e630
Anas <131682794+Anas1010101@users.noreply.github.com>
2025-06-10 10:48:10 +0200

    Removing this useless file

--- MartyProject/connexion_Deconnexion.py
+++ MartyProject/connexion_Deconnexion.py
@@ -1,16 +1,1 @@
-from martypy import Marty
 
-MARTY_IP = "192.168.0.102"
-
-def connexion():
-    try:
-        marty = Marty("wifi", MARTY_IP)
-        marty.dance()
-        print("Connexion Réussie à Marty.")
-    except Exception as e:
-        print(f"Erreur de connexion : {e}")
-
-if __name__ == "__main__":
-    connexion()
-
-    
